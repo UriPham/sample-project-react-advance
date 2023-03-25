647f5ee874b5206074e74857545ebf0ffea400bd
UriPham <ducphamtrung.devw@gmail.com>
2023-03-25 17:18:19 +0700

    #3

--- src/lessons/Lesson3/Lesson3.jsx
+++ src/lessons/Lesson3/Lesson3.jsx
@@ -1,9 +1,20 @@
+import { useState } from "react";
+import CoolButton from './CoolButton'
+
 const Lesson3 = (props) => {
+    const [name, setName] = useState('')
     return (
         <div>
             <label>Name:</label>
-            <input type={'text'} />
-            <div>Your name 's ... </div>
+            <input
+                onChange={e => setName(e.target.value)}
+                type={'text'}
+            />
+            <div>Your name 's {name} </div>
+            <CoolButton
+                btnClass="primary"
+                title="primary"
+            />
         </div>
     )
 }
